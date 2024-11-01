47c5903cb320e88ab0c023407696bd103708083a
Igor Grosu <igrgro@gmail.com>
2024-11-01 22:41:41 +0200

    Update user.py

--- user/models/user.py
+++ user/models/user.py
@@ -3,8 +3,8 @@ from django.db import models
 class User(models.Model):
   id = models.BigIntegerField('ID', primary_key=True)
   is_bot = models.BooleanField('Бот', default=False)
-  first_name = models.CharField('Имя', max_length=32)
-  last_name = models.CharField('Фамилия', max_length=32)
+  first_name = models.CharField('Имя', max_length=32, blank=True)
+  last_name = models.CharField('Фамилия', max_length=32, blank=True)
   username = models.CharField('Имя пользователя @', max_length=32, unique=True)
   language_code = models.CharField('Язык', default='en', max_length=2)
   added_to_attachment_menu = models.BooleanField('Добавил в меню прикрепленных', default=False)
